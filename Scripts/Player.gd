f1d459fd69bc5a08cbb68190dc64dab3bc12160d
Mokumokuren00 <mokumokugames@gmail.com>
2021-11-07 18:54:05 +0200

    Not an important change
    - Moved the code that does moving to a new method

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,6 +7,10 @@ const FRICTION = 500
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
+	move_state(delta)
+	pass
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -17,5 +21,4 @@ func _physics_process(delta):
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	move_and_slide(velocity);
-	pass
+	velocity = move_and_slide(velocity);

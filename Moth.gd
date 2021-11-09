b1a979a66c1ff1cf58fc6c889255d91ff7a522b7
eilfunn <93827815+eilfunn@users.noreply.github.com>
2021-11-08 21:39:35 -0500

    fixed moth position

--- Moth.gd
+++ Moth.gd
@@ -6,4 +6,4 @@ onready var moth = get_parent().get_node("Moth")
 onready var player = get_parent().get_node("Player")
 
 func _physics_process(delta):
-	moth.global_position = moth.global_position.linear_interpolate((player.get_global_position() + Vector2(0,-30)), delta * 4)
+	moth.global_position = moth.global_position.linear_interpolate((player.get_global_position() + Vector2(-30,-30)), delta * 4)

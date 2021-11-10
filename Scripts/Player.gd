166bbe0e28df63ad8134fc9d338f4373ffc6f45a
Mokumokuren00 <mokumokugames@gmail.com>
2021-11-10 18:51:36 +0200

    Housekeeping Update
    - Removed:
    -- Scenes that won't be used.
    -- Scripts that won't be used.
    -- Temp art that won't be used.
    -- Prefabs that won't be used.
    - Code editing:
    -- Alex's code has been commented out from the player's script. This way we can use it later.
    -- Changed friction value to 600
    -- Removed the look_direction variable as it was for the Pivot.gd script, which won't be used.
    - Map:
    -- Fixed a wall.
    - Starting point:
    -- Set to Mansion.tscn

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,35 +2,32 @@ extends KinematicBody2D
 
 const MAX_SPEED = 120
 const ACCELERATION = 900
-const FRICTION = 300
+const FRICTION = 600
 
 #variable from jsalex7
-const PLAYER_HOUSE_X = 250 #use this for set the spawning position when the player enters into a new place
-const PLAYER_HOUSE_Y = 230
+#const PLAYER_HOUSE_X = 250 #use this for set the spawning position when the player enters into a new place
+#const PLAYER_HOUSE_Y = 230
 
-onready var Global = get_node("/root/GlobalsOfDoom")
+#onready var Global = get_node("/root/GlobalsOfDoom")
 
-var is_it_start = true
+#var is_it_start = true
 #end of jsalex7
 var velocity = Vector2.ZERO
-var look_direction = Vector2()
 
-func _ready():
-	var scene_name = get_tree().get_current_scene().get_name()
-	if scene_name == "PlayerHouse" and !Global.is_it_start:
-		self.position = Vector2(self.PLAYER_HOUSE_X, self.PLAYER_HOUSE_Y)
-	Global.is_it_start = false
+#func _ready():
+#	var scene_name = get_tree().get_current_scene().get_name()
+#	if scene_name == "PlayerHouse" and !Global.is_it_start:
+#		self.position = Vector2(self.PLAYER_HOUSE_X, self.PLAYER_HOUSE_Y)
+#	Global.is_it_start = false
 
 func _physics_process(delta):
 	move_state(delta)
-	pass
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	look_direction = input_vector;
 	
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)

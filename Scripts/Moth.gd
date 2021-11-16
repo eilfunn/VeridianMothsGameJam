396c5f2b4200d5c8b2b37f0e932468f9e2dc2fd5
Mokumokuren00 <mokumokugames@gmail.com>
2021-11-16 23:14:06 +0200

    Revert "Decoration Update +"
    This reverts commit f86d656765f42352f27c43e631043d36dc8d3bf7.

--- Scripts/Moth.gd
+++ Scripts/Moth.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-onready var player = get_parent().get_node("YSort").get_node("Player")
+onready var moth = get_parent().get_node("Moth")
+onready var player = get_parent().get_node("Player")
 
 func _physics_process(delta):
-	global_position = global_position.linear_interpolate((player.get_global_position() + Vector2(-15,-15)), delta * 4)
+	moth.global_position = moth.global_position.linear_interpolate((player.get_global_position() + Vector2(-15,-15)), delta * 4)

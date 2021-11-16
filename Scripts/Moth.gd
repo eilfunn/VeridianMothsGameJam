f86d656765f42352f27c43e631043d36dc8d3bf7
Mokumokuren00 <mokumokugames@gmail.com>
2021-11-16 23:01:26 +0200

    Decoration Update +
    - Mainly decoration relate stuff.
    - Changed some stuff about the moth familiar too.

--- Scripts/Moth.gd
+++ Scripts/Moth.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-onready var moth = get_parent().get_node("Moth")
-onready var player = get_parent().get_node("Player")
+onready var player = get_parent().get_node("YSort").get_node("Player")
 
 func _physics_process(delta):
-	moth.global_position = moth.global_position.linear_interpolate((player.get_global_position() + Vector2(-15,-15)), delta * 4)
+	global_position = global_position.linear_interpolate((player.get_global_position() + Vector2(-15,-15)), delta * 4)
